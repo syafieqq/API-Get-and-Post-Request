2f2627800422ee62741711a878b914ff5797296f
Megat <syafiqq95@gmail.com>
2018-01-17 21:43:42 +0800

    add new file

--- api.php
+++ api.php
@@ -1,32 +1,27 @@
 <?php
- 
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
- 
+if (!$input) $input = array();
 // connect to the mysql database
 $link = mysqli_connect('localhost', 'root', '', 'api');
 mysqli_set_charset($link,'utf8');
- 
 // retrieve the table and key from the path
 $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 $key = array_shift($request)+0;
- 
 // escape the columns and values from the input object
-$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys((array)($input)));
+$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
 $values = array_map(function ($value) use ($link) {
   if ($value===null) return null;
   return mysqli_real_escape_string($link,(string)$value);
-},array_values ((array)($input)));
- 
+},array_values($input));
 // build the SET part of the SQL command
 $set = '';
 for ($i=0;$i<count($columns);$i++) {
   $set.=($i>0?',':'').'`'.$columns[$i].'`=';
   $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
 }
- 
 // create SQL based on HTTP method
 switch ($method) {
   case 'GET':
@@ -36,18 +31,15 @@ switch ($method) {
   case 'POST':
     $sql = "insert into `$table` set $set"; break;
   case 'DELETE':
-    $sql = "delete `$table` where id=$key"; break;
+    $sql = "delete from `$table` where id=$key"; break;
 }
- 
-// excecute SQL statement
+// execute SQL statement
 $result = mysqli_query($link,$sql);
- 
 // die if SQL statement failed
 if (!$result) {
   http_response_code(404);
-  die(mysqli_error());
+  die(mysqli_error($link));
 }
- 
 // print results, insert id or affected row count
 if ($method == 'GET') {
   if (!$key) echo '[';
@@ -60,6 +52,5 @@ if ($method == 'GET') {
 } else {
   echo mysqli_affected_rows($link);
 }
- 
 // close mysql connection
 mysqli_close($link);
